f876de8dc311df58282e434863c181e6d74cd827
Eduardo Hidalgo <eduardo.hidalgo@grupopromerica.com>
2023-03-16 19:35:16 -0600

    otra vez3

--- spring-data/src/main/java/com/pfcti/springdata/model/Cliente.java
+++ spring-data/src/main/java/com/pfcti/springdata/model/Cliente.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Table(name="cliente")
 @Setter
@@ -19,5 +21,15 @@ public class Cliente {
     @Column(length = 15)
     private String cedula;
     private String telefono;
+
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Direccion> direcciones;
+
+   // @OneToMany(mappedBy = "cliente")
+   //private List<Tarjeta> tarjetas;
+   // @OneToMany(mappedBy = "cliente")
+    //private List<Cuenta> cuentas;
+
 
 }
