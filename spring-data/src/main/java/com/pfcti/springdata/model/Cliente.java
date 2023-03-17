85a93e2a48fb6ede7ba93b55d03f3f5c94376a3a
Eduardo Hidalgo <eduardo.hidalgo@grupopromerica.com>
2023-03-16 18:36:26 -0600

    otra vez3

--- spring-data/src/main/java/com/pfcti/springdata/model/Cliente.java
+++ spring-data/src/main/java/com/pfcti/springdata/model/Cliente.java
@@ -1,9 +1,6 @@
 package com.pfcti.springdata.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +10,7 @@ import lombok.Setter;
 @Getter
 public class Cliente {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name="nombre")
     private String nombre;
